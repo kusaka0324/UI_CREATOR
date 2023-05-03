47eae2bdd9ebe4b278b4305fc2f68c2aa2af4974
kusaka0324 <haruto.k.4230@gmail.com>
2023-05-04 02:43:04 +0900

    feat : 画面遷移のルーティング設定

---
+++ src/router.jsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+
+import { 
+  SetBackground,
+  SetDisplay,
+  SetStyling,
+  SetCustom, 
+} from "./components/pages";
+
+export default function Router(){
+  return (
+    <Routes>
+      <Route path={'/'}           element={ <SetBackground/> } />
+      <Route path={'/display'}    element={ <SetDisplay/> } />
+      <Route path={'/styling'}    element={ <SetStyling/> } />
+      <Route path={'/set_custom'} element={ <SetCustom/> } />
+    </Routes>
+  )
+}
