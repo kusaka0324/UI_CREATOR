a1a6ca816008cbdb4e5926f048fd3f8f8d27da63
kusaka0324 <haruto.k.4230@gmail.com>
2023-05-04 02:42:37 +0900

    feat : react-router-domのprovider導入

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Router from './router'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
   </React.StrictMode>,
 )
