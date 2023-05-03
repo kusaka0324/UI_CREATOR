4f511d6a4623f6c727aba66a438b0f9d983e6058
kusaka0324 <haruto.k.4230@gmail.com>
2023-05-04 02:46:19 +0900

    add : initial commit

---
+++ src/components/pages/SetBackground/SetBackground.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Header, Sidebar } from '../../ui'
+
+export const SetBackground = () => {
+  return (
+    <div>
+      <Header/>
+      <Sidebar/>
+    </div>
+  )
+}
+
+
---
+++ src/components/pages/SetCustom/SetCustom.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const SetCustom= () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
