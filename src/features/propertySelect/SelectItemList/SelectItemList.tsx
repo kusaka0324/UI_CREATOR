52ab91e964ddf567b812f6639f75b3d7e86c23b3
kusaka0324 <haruto.k.4230@gmail.com>
2023-05-04 17:30:32 +0900

    add : initial commit

---
+++ src/features/propertySelect/SelectItemList/SelectItemList.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const SelectItemList = ({children}) => {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
---
+++ src/features/propertySelect/SelectItemList/propertyList.js
@@ -0,0 +1,42 @@
+export const propertyList = [
+  {
+    id: 0,
+    propertyName: 'width',
+  },
+  {
+    id: 1,
+    propertyName: 'height',
+  },
+  {
+    id: 2,
+    propertyName: 'font-size',
+  },
+  {
+    id: 3,
+    propertyName: 'font-weight',
+  },
+  {
+    id: 4, 
+    propertyName: 'box-shadow',
+  },
+  {
+    id: 5,
+    propertyName: 'background',
+  },
+  {
+    id: 6,
+    propertyName: 'color',
+  },
+  {
+    id: 7,
+    propertyName: 'border-radius',
+  },
+  {
+    id: 8,
+    propertyName: 'border',
+  },
+  {
+    id: 9,
+    propertyName: ':active',
+  }
+];
