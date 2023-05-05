5dbf9a786a49bead99664ee91c80909442527746
kusaka0324 <haruto.k.4230@gmail.com>
2023-05-05 18:58:20 +0900

    feat : dndを有効化するためのプロバイダ設定

--- src/providers/EnableDndProvider.jsx
+++ src/providers/EnableDndProvider.jsx
@@ -1,1 +1,11 @@
+import React from 'react';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { DndProvider } from 'react-dnd';
 
+export const EnableDndProvider = ({ children }) => {
+  return (
+    <DndProvider backend={ HTML5Backend } >
+      {children}
+    </DndProvider>
+  )
+}
