6287ea3336fe0febc326b4c87a0a2916dae57e79
kusaka0324 <haruto.k.4230@gmail.com>
2023-05-05 18:57:29 +0900

    delete

--- src/providers/EnableDndProvider.jsx
+++ src/providers/EnableDndProvider.jsx
@@ -1,12 +1,1 @@
-import React from 'react';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-import { DndProvider } from 'react-dnd';
-
-export const EnableDndProvider = ({ children }) => {
-  return (
-    <DndProvider backend={ HTML5Backend } >
-      {children}
-    </DndProvider>
-  )
-}
 
