a0580ddb492c30dc52f71dd5959635ffb91db216
kusaka0324 <haruto.k.4230@gmail.com>
2023-05-06 16:05:27 +0900

    rename : ファイル名の意味が不適切なため変更

--- src/components/ui/Topber.jsx
+++ src/components/ui/Topber.jsx
@@ -1,7 +1,7 @@
 import React  from 'react';
 import styled from 'styled-components';
 
-export const Header = ({ title, accentTitle }) => {
+export const Topber = ({ title, accentTitle }) => {
   return (
     <HeaderStyle>
       <HeaderTitle>
@@ -17,7 +17,7 @@ export const Header = ({ title, accentTitle }) => {
 
 const HeaderStyle =styled.header`
   position  : relative;
-  height    : 80px;
+  height    : 5.75rem;
   width     : 100%;
   background: #fefefe;
   box-shadow: 3px 3px 3px 1.5px rgb(199, 199, 199);
@@ -26,15 +26,15 @@ const HeaderStyle =styled.header`
 const HeaderTitle = styled.div`
   position   : absolute;
   top        : 50%;
-  left       : 50px;
+  left       : 4.75rem;
   transform  : translateY(-50%);
-  line-height: 32px;
-  font-size  : 32px;
+  line-height: 3.25rem;
+  font-size  : 3.25rem;
   font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
   font-weight: bold;
   
   > span {
-    padding-left: 5px;
-    font-size   : 24px;
+    padding-left: 0.25rem;
+    font-size   : 2rem;
   }
 `;
