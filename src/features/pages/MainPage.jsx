02c033f2266e7f4775f76dd4640452bc099d8367
kusaka0324 <haruto.k.4230@gmail.com>
2023-05-07 05:07:49 +0900

    feat : sidebarのナビゲーション実装

--- src/features/pages/MainPage.jsx
+++ src/features/pages/MainPage.jsx
@@ -3,9 +3,19 @@ import { MainLayout, ContentLayout } from '@/components/layouts';
 import { ScreenLayout }  from '@/features/screen';
 import { EditorLayout }  from '@/features/codeEditor';
 import { PreviewLayout } from '@/components/layouts/PreviewLayout';
-import { Topber }        from '@/components/ui';
+import { IconButton, Topber } from '@/components/ui';
+import { SelectAreaLayout } from '../propertySelect';
+import styled from 'styled-components';
+import CodeIcon     from '@mui/icons-material/Code';
+import GridViewIcon from '@mui/icons-material/GridView';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 export const MainPage = () => {
+  const [activeContent, setActiveContent] = React.useState('codeEditor');
+
+  function handleButtonClick(contentId) {
+    setActiveContent(contentId);
+  }
   return (
     <MainLayout >
       <Topber title={'UI'} accentTitle={'CREATER'} />
@@ -13,9 +23,54 @@ export const MainPage = () => {
         <ScreenLayout ></ScreenLayout>        
       </PreviewLayout>
       <ContentLayout >
-        <EditorLayout></EditorLayout>   
+        <Navigation >
+          <NavList >
+            <IconButton 
+              icon   = { <CodeIcon/> }
+              onClick= { () => handleButtonClick('codeEditor') } 
+              active = { activeContent==='codeEditor' } 
+            />
+            <IconButton 
+              icon   = { <GridViewIcon/> }
+              onClick= { () => handleButtonClick('displayButtons') }
+              active = { activeContent==='displayButtons' } 
+            />
+            <IconButton 
+              icon   = { <SettingsIcon/> }
+              onClick= { () => handleButtonClick('setCustom') } 
+              active = { activeContent==='setCustom' }
+            />
+          </NavList>
+        </Navigation>
+      { activeContent === 'codeEditor' && (
+          <EditorLayout></EditorLayout>
+        )
+      }
+      { activeContent === 'displayButtons' && (
+          <SelectAreaLayout>
+            
+          </SelectAreaLayout>
+        )
+      }
       </ContentLayout>        
     </MainLayout>
   )
 }
 
+const Navigation= styled.ul`
+  width        : 3.75rem;
+  height       : 100%;
+  background   : #2B2B2B;
+  opacity      : 0.8;
+  list-style : none;
+`;
+
+const NavList= styled.li`
+  display        : flex;
+  flex-direction : column;
+  align-items    : center;
+  height         : 100%;
+  width          : 100%;
+  color          :#FFFDFD;
+  text-decoration: none;
+`;
