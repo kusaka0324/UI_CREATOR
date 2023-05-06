af382f79fcfe3242ea2cced62a6de5703e1905b9
kusaka0324 <haruto.k.4230@gmail.com>
2023-05-07 05:00:42 +0900

    move : ディレクトリ変更

--- src/components/layouts/ContentLayout.jsx
+++ src/components/layouts/ContentLayout.jsx
@@ -26,23 +26,3 @@ const ContentWrapper= styled.div`
   width   : 100%;
   padding : 1rem;
 `;
-
-const ContentTitle= styled.div`
-  position   : absolute;
-  top        : 1rem;
-  left       : 1.75rem;
-  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-  font-weight: 600;
-  font-size  : 2rem;
-  color      : #2424dc;
-  z-index    : 100;
-
-  > div {
-    margin-top : 0.5rem;
-    font-size  : 1rem;
-    font-weight: 500;
-    color      : #a9a9a9;
-  }
-`;
-
-const ContentStyle= styled.div``;
