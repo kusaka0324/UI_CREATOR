3d5c6064c540b34f3d3a0c48c45cb42a2203fd50
kusaka0324 <haruto.k.4230@gmail.com>
2023-05-06 19:19:02 +0900

    feat : ページ遷移時のコストが高いため、不要ページを削除し、ページを絞る

--- src/router.jsx
+++ src/router.jsx
@@ -1,19 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 
-import { 
-  SetBackground,
-  SetDisplay,
-  SetStyling,
-  SetCustom, 
-} from "./features/pages";
+import { MainPage, WebviewControllerPage } from './features/pages';
 
 export default function Router(){
   return (
     <Routes>
-      <Route path={'/'}           element={ <SetBackground/> } />
-      <Route path={'/display'}    element={ <SetDisplay/> } />
-      <Route path={'/styling'}    element={ <SetStyling/> } />
-      <Route path={'/set_custom'} element={ <SetCustom/> } />
+      <Route path={'/'}           element={ <MainPage /> } />
+      <Route path={'/webview-controller'}    element={ <WebviewControllerPage/> } />
     </Routes>
   )
 }
