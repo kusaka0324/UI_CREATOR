5fc2840a47e47954bdb3388d23a815dbeab4ded3
kusaka0324 <haruto.k.4230@gmail.com>
2023-05-21 15:46:02 +0900

    feat :  作成したロゴを追加

--- src/components/ui/Topber.jsx
+++ src/components/ui/Topber.jsx
@@ -1,14 +1,13 @@
 import React  from 'react';
 import styled from 'styled-components';
 
-export const Topber = ({ title, accentTitle }) => {
+import logo from '@/assets/logo.svg'
+
+export const Topber = () => {
   return (
     <HeaderStyle>
       <HeaderTitle>
-        {title}
-        <span >
-          {accentTitle}
-        </span> 
+        <img src={logo}/>
       </HeaderTitle>
     </HeaderStyle>
   )
@@ -18,25 +17,21 @@ export const Topber = ({ title, accentTitle }) => {
 const HeaderStyle =styled.header`
   grid-row   : 1;
   grid-column: 1 / span 2;
-  position  : relative;
-  height    : 5.75rem;
-  background: #1c1c1c;
+  position   : relative;
+  height     : 5.75rem;
+  background : #fefefe;
+  box-shadow : 10px 0px 10px rgba(0, 0, 0, 0.25);
 `;
 
 const HeaderTitle = styled.div`
   position   : absolute;
-  top        : 50%;
-  left       : 4.75rem;
+  top        : 50%;  
   transform  : translateY(-50%);
+  left       : 2.75rem;
   line-height: 3.25rem;
-  color: #251be8;
-  font-size  : 3.25rem;
-  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-  font-weight: bold;
-  
-  > span {
-    color:white;
-    padding-left: 0.25rem;
-    font-size   : 2rem;
+  > img {
+    height: 5.25rem;
+    display: flex;
+    align-items: center;
   }
 `;
