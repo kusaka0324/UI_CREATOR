1eb234bb54686d3250dccdfb19a036a3e7a5261e
kusaka0324 <haruto.k.4230@gmail.com>
2023-05-21 15:48:40 +0900

    feat : プロパティのリスト追加

---
+++ src/features/codeEditor/properties/propsList.js
@@ -0,0 +1,46 @@
+export const propsList= [
+  {
+    id: 0,
+    propsName: 'width',
+    unit     : 'px',
+  },
+  {
+    id: 1,
+    propsName: 'height',
+    unit     : 'px',
+  },
+  {
+    id: 2,
+    propsName: 'background',
+    unit     : 'color-code',
+  },
+  {
+    id: 3,
+    propsName: 'color',
+    unit     : 'color-code',
+  },
+  {
+    id: 4,
+    propsName: 'font-size',
+    unit     : 'px',
+  },   
+  {
+    id: 5,
+    propsName: 'border',
+    unit     : 'confusion',
+  },
+  {
+    id: 6,
+    propsName: 'border-radius',
+    unit     : 'px',
+  },  
+  {
+    id: 7,
+    propsName: 'box-shadow',
+    unit     : 'confusion',
+  },
+  {
+    id: 8,
+    propsName: 'active',
+  },
+];
