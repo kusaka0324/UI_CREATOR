2d66d5d72309757d424de2cdbe70a808e8b7caba
kusaka0324 <haruto.k.4230@gmail.com>
2023-05-24 09:55:11 +0900

    style : プロパティのリストスタイル変更

--- src/features/codeEditor/properties/SelectCssProps.jsx
+++ src/features/codeEditor/properties/SelectCssProps.jsx
@@ -8,45 +8,68 @@ import { PropItem } from './PropItem';
 export const SelectCssProps= () => {
   return (
     <SelectAreaLayout>
-      <SelectAreaTitle>CSS Properties</SelectAreaTitle>
+      <SelectAreaTitle>
+        
+        CSS Properties
+      </SelectAreaTitle>
       
       <SelectAreaWrapper>
-        <ul>
-          {propsList.map(({id, propName, unit}) => (
-            <DraggableLayer key={id}>
-              <li>
+        <PropItemList>
+          {propsList.map(({id, propName, explanation, unit}) => (
+            <li key={id} >
+              <ExpStyle>{explanation}</ExpStyle>
+              <DraggableLayer >
                 <PropItem 
                   propName={propName} 
                   unit={unit}
                 />
-              </li>              
-            </DraggableLayer>
+              </DraggableLayer>
+            </li>              
           ))}          
-        </ul>
+        </PropItemList>
       </SelectAreaWrapper>
     </SelectAreaLayout>
   );
 };
 
 const SelectAreaLayout= styled.section`
-  position  : absolute;
-  bottom    : 0;
-  height    : 50%;
-  width     : 100%;
-  background: #1c1c1c;
+  position     : absolute;
+  top          : 0;
+  right        : 0;
+  height       : 100%;
+  width        : 50%;
+  background   : #1c1c1c;
+  border-radius: 0 20px 20px 0;
 `;
 
 const SelectAreaTitle= styled.div`
-  color      : #878787;
+  display    : flex;
+  align-items: center;
+  color      : #6129FF;
   font-size  : 1.25rem;
   font-weight: 600;
+  height     : 50px;
 `;
 
 const SelectAreaWrapper= styled.div`
-  display : grid;
-  padding : 1rem 2rem 2rem 1rem;
-  width   : 100%;
-  height  : 100%;
-  opacity : 0.7;
-  background : black;
+  padding   : 1rem 2rem 2rem 1rem;
+  width     : 100%;
+  height    : calc(100% - 3.15rem);
+  background: #1e1e1e;
+  overflow  : auto;
+`;
+
+const PropItemList= styled.ul`
+  list-style    : none;
+  display       : flex;
+  flex-direction: column;
+  align-items   : center;
+  gap: 2.25rem;
+`;
+
+const ExpStyle= styled.span`
+  color      : #929292;
+  font-size  : 0.75rem;
+  font-weight: 500;
+  
 `;
