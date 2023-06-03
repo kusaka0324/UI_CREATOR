84f391c1a34c865c494f6b5c36758f76c0781118
kusaka0324 <haruto.k.4230@gmail.com>
2023-06-04 00:56:42 +0900

    delete

--- src/components/ui/Drawer.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Drawer = ({openIcon, closeIcon, children}) => {
-  return (
-    <div>{children}</div>
-  )
-}
--- src/config/index.js
+++
@@ -1,1 +0,0 @@
-export const API_URL = process.env.UI_CREATOR_API_URL;
