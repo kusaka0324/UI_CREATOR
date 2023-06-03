7f1eb97ffa819b1d3a388b84efae60ecd3c34f07
kusaka0324 <haruto.k.4230@gmail.com>
2023-06-04 00:57:09 +0900

    refactor : インポートの整形

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import reactRefresh from '@vitejs/plugin-react-refresh';
+import react            from '@vitejs/plugin-react';
+import reactRefresh     from '@vitejs/plugin-react-refresh';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react(), reactRefresh()],
