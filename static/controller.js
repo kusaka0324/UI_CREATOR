731daf56d81f33fa1009591cc311c9401d7260c8
ORIKAMIxNEP <ta.orikami@gmail.com>
2023-06-23 10:31:47 +0900

    ファイル復元

---
+++ static/controller.js
@@ -0,0 +1,41 @@
+const API_URL = "http://54.249.130.95/controller/change-status";
+const controller = document.title.slice(-1);
+
+const button = [
+  "leftForward",
+  "leftReverse",
+  "rightForward",
+  "rightReverse",
+  "speedBoost",
+  "rightTurn",
+  "leftTurn",
+];
+let buttonElements = [];
+for (let i = 0; i < button.length; i++) {
+  buttonElements.push(document.getElementById(button[i] + "Button"));
+}
+for (let i = 0; i < button.length; i++) {
+  if (buttonElements[i] !== null) {
+    if (i < 5) {
+      buttonElements[i].addEventListener("touchstart", function () {
+        toggleButton(controller, i);
+      });
+      buttonElements[i].addEventListener("touchend", function () {
+        toggleButton(controller, i);
+      });
+    } else {
+      buttonElements[i].addEventListener("touchstart", function () {
+        toggleButton(controller, i - 5);
+        toggleButton(controller, i - 3);
+      });
+      buttonElements[i].addEventListener("touchend", function () {
+        toggleButton(controller, i - 5);
+        toggleButton(controller, i - 3);
+      });
+    }
+  }
+}
+
+function toggleButton(controller, button) {
+  fetch(API_URL + "?controller=" + controller + "&button=" + button);
+}
