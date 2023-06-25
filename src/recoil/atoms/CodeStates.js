1a04dba9a8acbd7287b41af7e9c2cea7d8512d2c
kusaka0324 <haruto.k.4230@gmail.com>
2023-06-26 01:03:49 +0900

    rename : ファイル名変更

--- src/recoil/atoms/CodeStates.js
+++ src/recoil/atoms/CodeStates.js
@@ -15,3 +15,9 @@ export const EditByHtmlAtom = atom({
   </body>
 </html>`
 });
+
+export const EditByCssAtom = atom({
+  key    : 'edit-css-state',
+  default: 
+`html{position:relative;height:100%;width:100%;}`
+})
