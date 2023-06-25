063184fe7eb5010bbeced1bcf3d9746b58d5ee7c
kusaka0324 <haruto.k.4230@gmail.com>
2023-06-25 15:48:30 +0900

    feat : apiのurlをインポート

--- src/features/preview/api/postCustom.js
+++ src/features/preview/api/postCustom.js
@@ -1,8 +1,9 @@
 import axios from "axios";
-import { API_URL } from "@/config";
 
-export const postCustom = async ({data}) => {
-    axios.post(API_URL, {data})
+import { CONTROLLER_API_URL } from "@/config";
+
+export async function postCustom ({data}) {
+    axios.post(CONTROLLER_API_URL, {data})
     .then(response => {
 			console.log(response);
     })
