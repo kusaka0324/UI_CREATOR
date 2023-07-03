638e3148669f4d921560c77bb034e692b8fa6182
kusaka0324 <haruto.k.4230@gmail.com>
2023-07-04 05:40:29 +0900

    refactor : 初期値変更

--- src/recoil/atoms/CodeStates.js
+++ src/recoil/atoms/CodeStates.js
@@ -2,22 +2,21 @@ import { atom } from 'recoil';
 
 export const EditByHtmlAtom = atom({
   key    : 'edit-html-state',
-  default:
-`<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Controller</title>
-  </head>
-  <body>
-  </body>
-</html>`
+  default: '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Controller</title></head><body></body></html>'
 });
 
 export const EditByCssAtom = atom({
   key    : 'edit-css-state',
   default: 
-`html{position:relative;height:100%;width:100%;}`
-})
+`*{
+  box-sizing:border-box;
+  margin:0;
+  padding:0;
+}
+html{
+  position:relative;
+  height:100%;
+  width:100%;
+}
+`,
+});
