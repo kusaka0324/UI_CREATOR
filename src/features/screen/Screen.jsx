9f1c7d0dd94cde112e4b561823412190d27ff7ce
kusaka0324 <haruto.k.4230@gmail.com>
2023-07-07 03:07:07 +0900

    style : スクリーンのサイズ変更

--- src/features/screen/Screen.jsx
+++ src/features/screen/Screen.jsx
@@ -1,10 +1,9 @@
-import React  from 'react'
+import React           from 'react'
 import styled, { css } from 'styled-components';
 
-import { DroppableScreen } from '../controller';
-import { useRecoilValue } from 'recoil';
+import { DroppableScreen }   from '../controller';
+import { useRecoilValue }    from 'recoil';
 import { MergeCodeSelector } from '@/recoil/selector';
-import { EditByCssAtom } from '@/recoil/atoms';
 
 export const Screen = ({ mode }) => {
   const mergeHtml= useRecoilValue(MergeCodeSelector);
@@ -12,7 +11,8 @@ export const Screen = ({ mode }) => {
   return (
     <ScreenStyle mode={mode} >
         <ScreenContent >
-          <iframe srcDoc={mergeHtml}></iframe>
+          <DroppableScreen />
+          <iframe srcDoc={mergeHtml} />
         </ScreenContent>        
     </ScreenStyle>
   );
@@ -26,14 +26,14 @@ const ScreenStyle = styled.div`
   ${(props)=> 
     props.mode === 'preview' 
     ? css`
-        padding: 15px 30px 15px 30px;
-        height : 50vh;
-        width  : 100vh;       
+        padding: 10px 40px 10px 40px;
+        height : 60vh;
+        width  : 120vh;       
       `
     : css`
-        padding: 10px 20px 10px 20px;
-        height : 35vh;
-        width  : 70vh;       
+        padding: 10px 30px 10px 30px;
+        height : 45vh;
+        width  : 90vh;       
     `
     };
 `;
@@ -43,15 +43,19 @@ const ScreenContent = styled.div`
   top          : 50%;
   left         : 50%;
   background   : #f0efef;
-  border-radius: 30px;
   transform    : translate(-50%, -50%);
   height       : 100%;
   width        : 100%;
+  > div {
+    position     : absolute;
+    width        : 100%;
+    height       : 100%;
+    border-radius: 30px;
+  }
 
   > iframe {
-    width : 100%;
-    height: 100%;
-    border: none;
-    border-radius: 30px;
+    width        : 100%;
+    height       : 100%;
+    border       : none;
   }
 `;
