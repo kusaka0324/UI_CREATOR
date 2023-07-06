2bab2773011c96e4b0c65c362c6e941a3d87c978
kusaka0324 <haruto.k.4230@gmail.com>
2023-07-07 03:23:05 +0900

    feat : エディタの初期状態を設定

--- src/recoil/atoms/CodeStates.js
+++ src/recoil/atoms/CodeStates.js
@@ -2,7 +2,18 @@ import { atom } from 'recoil';
 
 export const EditByHtmlAtom = atom({
   key    : 'edit-html-state',
-  default: '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Controller</title></head><body></body></html>'
+  default: 
+`<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Controller</title>
+  </head>
+  <body></body>
+</html>
+`
 });
 
 export const EditByCssAtom = atom({
@@ -15,8 +26,8 @@ export const EditByCssAtom = atom({
 }
 html{
   position:relative;
-  height:100%;
-  width:100%;
+  height:100vh;
+  width:100vw;
 }
 `,
 });
