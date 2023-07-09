ad6b446d1a44c828396d3a58e967c8ffbc9c68d9
kusaka0324 <haruto.k.4230@gmail.com>
2023-07-10 05:29:46 +0900

    add : svgのスタイルをあてるための、css追加

--- src/data/cssStyle.js
+++ src/data/cssStyle.js
@@ -2,36 +2,36 @@ export const cssStyle= [
 	{
 		id: 0,
 		className   : 'leftForward',
-		defaultStyle: `#leftForward{position:absolute;top:0%;left:0%;width:60px;height:60px;background:grey;}`
+		defaultStyle: `#leftForward{position:absolute;top:0%;left:0%;width:60px;height:60px;background:grey;& svg{color:black;width:16px;}}`
 	},
 	{
 		id: 1,
 		className   : 'leftReverse',
-		defaultStyle: `#leftReverse{position:absolute;top:0%;left:10%;width:60px;height:60px;background:grey;}`
+		defaultStyle: `#leftReverse{position:absolute;top:0%;left:10%;width:60px;height:60px;background:grey;& svg{color:black;width:16px;}}`
 	},
 	{
 		id: 2,
 		className   : 'rightForward',
-		defaultStyle: `#rightForward{position:absolute;top:0%;left:20%;width:60px;height:60px;background:grey;}`
+		defaultStyle: `#rightForward{position:absolute;top:0%;left:20%;width:60px;height:60px;background:grey;& svg{color:black;width:16px;}}`
 	},
 	{
 		id: 3,
 		className   : 'rightReverse',
-		defaultStyle: `#rightReverse{position:absolute;top:0%;left:30%;width:60px;height:60px;background:grey;}`
+		defaultStyle: `#rightReverse{position:absolute;top:0%;left:30%;width:60px;height:60px;background:grey;& svg{color:black;width:16px;}}`
 	},
 	{
 		id: 4,
 		className   : 'speedBoost',
-		defaultStyle: `#speedBoost{position:absolute;top:0%;left:40%;width:60px;height:60px;background:grey;}`
+		defaultStyle: `#speedBoost{position:absolute;top:0%;left:40%;width:60px;height:60px;background:grey;& svg{color:black;width:16px;}}`
 	},
 	{
 		id: 5,
 		className   : 'leftTurn',
-		defaultStyle: `#leftTurn{position:absolute;top:0%;left:50%;width:60px;height:60px;background:grey;}`
+		defaultStyle: `#leftTurn{position:absolute;top:0%;left:50%;width:60px;height:60px;background:grey;& svg{color:black;width:16px;}}`
 	},
 	{
 		id: 6,
 		className   : 'rightTurn',
-		defaultStyle: `#rightTurn{position:absolute;top:0%;left:60%;width:60px;height:60px;background:grey;}`
+		defaultStyle: `#rightTurn{position:absolute;top:0%;left:60%;width:60px;height:60px;background:grey;& svg{color:black;width:16px;}}`
 	},
 ];
