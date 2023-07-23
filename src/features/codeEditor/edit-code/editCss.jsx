b4cd0d12b86fc27be21755b8c712e0f0b9724222
kusaka0324 <haruto.k.4230@gmail.com>
2023-07-23 09:57:08 +0900

    fix : コードの状態変更ごとに前の状態のコードが反映されるバグを修正

--- src/features/codeEditor/edit-code/editCss.jsx
+++ src/features/codeEditor/edit-code/editCss.jsx
@@ -1,24 +1,22 @@
 import React              from 'react';
-import { useRecoilState, useRecoilValue } from 'recoil';
+import { useRecoilState } from 'recoil';
 import CodeMirror         from '@uiw/react-codemirror';
 import { vscodeDark }     from '@uiw/codemirror-theme-vscode';
 import { css }            from '@codemirror/lang-css';
 import { color }          from '@uiw/codemirror-extensions-color';
 
 import { CssFormatSelector } from '@/recoil/selector';
-import { EditByCssAtom } from '@/recoil/atoms';
-import { defaultCss } from '@/data';
 
 export const EditCss = () => {
   const [ formattedCss, setFormattedCss ]= useRecoilState(CssFormatSelector);
 
   const handleChange = (value) => {
-    setFormattedCss(value.replace(defaultCss,''));
+    setFormattedCss(value);
   };
   
   return (
 		<CodeMirror
-			value     = { defaultCss+formattedCss }
+			value     = { formattedCss }
       onChange  = { handleChange }
       theme     = { vscodeDark }
       extensions= {[
