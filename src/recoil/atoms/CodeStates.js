4ec4a69fc7f001c60ed56960f174271a8d3023e9
kusaka0324 <haruto.k.4230@gmail.com>
2023-07-24 00:50:45 +0900

    fix : デフォルトの値を修正

--- src/recoil/atoms/CodeStates.js
+++ src/recoil/atoms/CodeStates.js
@@ -2,32 +2,10 @@ import { atom } from 'recoil';
 
 export const EditByHtmlAtom = atom({
   key    : 'edit-html-state',
-  default: 
-`<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Controller</title>
-  </head>
-  <body></body>
-</html>
-`
+  default: `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Controller</title></head><body></body></html>`
 });
 
 export const EditByCssAtom = atom({
   key    : 'edit-css-state',
-  default: 
-`*{
-  box-sizing:border-box;
-  margin:0;
-  padding:0;
-}
-html{
-  position:relative;
-  height:100vh;
-  width:100vw;
-}
-`,
+  default: `*{box-sizing:border-box;margin:0;padding:0;}html{position:relative;height:100vh;width:100vw;}`,
 });
