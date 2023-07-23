83a07c97336383927275615f0c4caf8d18240bdc
kusaka0324 <haruto.k.4230@gmail.com>
2023-07-24 00:49:03 +0900

    refactor : 値を読みこむだけなので、recoilの指定を変更

--- src/features/codeEditor/edit-code/editHtml.jsx
+++ src/features/codeEditor/edit-code/editHtml.jsx
@@ -1,5 +1,5 @@
 import React              from 'react';
-import { useRecoilState } from 'recoil';
+import { useRecoilValue } from 'recoil';
 import CodeMirror         from '@uiw/react-codemirror';
 import { vscodeDark }     from '@uiw/codemirror-theme-vscode';
 import { html }           from '@codemirror/lang-html';
@@ -8,12 +8,11 @@ import { HtmlFormatSelector } from '@/recoil/selector';
 import { defaultHtml } from '@/data';
 
 export const EditHtml = () => {    
-  const [ htmlCode, setHtmlCode ] = useRecoilState(HtmlFormatSelector);
+  const htmlCode= useRecoilValue(HtmlFormatSelector);
   
   return (
 		<CodeMirror
 			value     = { defaultHtml.replace('<body></body>', `<body>\n${htmlCode}</body>`) }
-      onChange  = { handleChange }
       theme     = { vscodeDark }
       extensions= { html() }
       readOnly  = { true }
