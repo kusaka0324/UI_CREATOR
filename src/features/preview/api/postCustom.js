736e2e5f27006f2a25d2fdb979496ef2fc34a14e
kusaka0324 <haruto.k.4230@gmail.com>
2023-07-24 00:32:52 +0900

    refactor : jsonデータを送信するため、content-typeを変更

--- src/features/preview/api/postCustom.js
+++ src/features/preview/api/postCustom.js
@@ -1,13 +1,16 @@
 import axios from "axios";
-
 import { CONTROLLER_API_URL } from "@/config";
 
-export async function postCustom ({data}) {
-    axios.post(CONTROLLER_API_URL, {data})
-    .then(response => {
-			console.log(response);
-    })
-    .catch(error => {
-			alert('通信エラー');
-    });
+export async function postCustom(data) {
+  axios.post(CONTROLLER_API_URL, {data}, {
+    headers: {
+      'Content-Type': 'application/json', 
+    },
+  })
+  .then(response => {
+		console.log(response);
+  })
+  .catch(error => {
+		alert('通信エラー');
+  });
 };
