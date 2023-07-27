c1da97a3e29630b48967f70824c5823ebaed272d
kusaka0324 <haruto.k.4230@gmail.com>
2023-07-28 02:17:36 +0900

    fix : urlを絶対パスで指定

--- src/features/preview/api/postCustom.js
+++ src/features/preview/api/postCustom.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export async function postCustom(data) {
-  axios.post('/controller', {data}, {
+  axios.post("http://54.249.130.95/controller/", {data}, {
     headers: {
       'Content-Type': 'application/json', 
     },
