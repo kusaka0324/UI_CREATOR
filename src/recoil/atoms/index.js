a0196d896c0c0d8c2287e2624942c76eac5f07c5
kusaka0324 <haruto.k.4230@gmail.com>
2023-07-28 01:42:33 +0900

    delete : 不要な呼び出しのため削除

--- src/recoil/atoms/index.js
+++ src/recoil/atoms/index.js
@@ -2,4 +2,3 @@ export * from './CodeStates';
 export * from './IncludeButtonsIdState';
 export * from './SelectedControllerState';
 export * from './SelectedButtonsState';
-export * from './EditErrorState';
