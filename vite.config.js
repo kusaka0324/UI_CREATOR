eff29d64ad50bd475bb7c76a50b54e3ecf303fd7
kusaka0324 <haruto.k.4230@gmail.com>
2023-07-28 11:03:56 +0900

    fix : 設定変更

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import react from '@vitejs/plugin-react'
+import reactRefresh     from '@vitejs/plugin-react-refresh';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [reactRefresh()],
   resolve: {
     alias: {
       '@': '/src'
