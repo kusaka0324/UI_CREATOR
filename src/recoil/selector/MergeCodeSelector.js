83361df41b1c9c723feb72e628def097e44ae533
kusaka0324 <haruto.k.4230@gmail.com>
2023-08-04 01:47:35 +0900

    fix : styleタグがheadタグ内に記述されるように変更

--- src/recoil/selector/MergeCodeSelector.js
+++ src/recoil/selector/MergeCodeSelector.js
@@ -1,13 +1,15 @@
 import { selector } from "recoil";
 import { EditByCssAtom, EditByHtmlAtom } from "../atoms";
+import { defaultHtml } from "@/data";
 
 export const MergeCodeSelector= selector({
 	key: 'merge-html-css',
 	get: ({get}) => {
 		const htmlState= get(EditByHtmlAtom);
 		const cssState = get(EditByCssAtom);
-		const insertIndex = htmlState.indexOf('</head>');
-		const updatedHtmlString = htmlState.slice(0, insertIndex) + `<style>${cssState}</style>` + htmlState.slice(insertIndex);
+		const formattedHtml = defaultHtml.replace('<body></body>', `<body>\n${htmlState}</body>`)
+		const insertIndex = formattedHtml.indexOf('</head>');
+		const updatedHtmlString = formattedHtml.slice(0, insertIndex) + `<style>${cssState}</style>` + formattedHtml.slice(insertIndex);
 
 		return updatedHtmlString;
 	}
