bd5d4bdd0c9e8c52a93e49ddebb661d357ea10cc
ORIKAMIxNEP <ta.orikami@gmail.com>
2023-08-03 13:53:47 +0900

    テスト用コードの追加

--- static/controller.js
+++ static/controller.js
@@ -18,7 +18,7 @@ for (let i = 0; i < button.length; i++) {
   console.log(i)
   if (buttonElements[i] !== null) {
     if (i < 5) {
-      buttonElements[i].addEventListener("onclick", function () {
+      buttonElements[i].addEventListener("click", function () {
         toggleButton(controller, i, 1);
         console.log("start")
       });
